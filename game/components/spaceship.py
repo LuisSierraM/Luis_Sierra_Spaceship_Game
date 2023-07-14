1cab6954439ee0e16474ba3cf7fa93232a728679
LuisSierraM <luisalfonsosierra09@gmail.com>
2023-07-14 12:52:23 -0500

    FIRST-CLASS-HOMEWORK

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -15,18 +15,23 @@ class Spaceship(Sprite):
         if user_input[pygame.K_LEFT]:
             if self.rect.left > 0:
                 self.rect.x -= 10
+            else:
+                self.rect.x = SCREEN_WIDTH - 40 
+
         elif user_input[pygame.K_RIGHT]:
             if self.rect.right < SCREEN_WIDTH:
                 self.rect.x += 10
+            else:
+                self.rect.x = 0  
+
         elif user_input[pygame.K_UP]:
             if self.rect.y > SCREEN_HEIGHT // 2:
                 self.rect.y -= 10
+                
         elif user_input[pygame.K_DOWN]:
-            if self.rect.y < SCREEN_HEIGHT -70:
+            if self.rect.y < SCREEN_HEIGHT - 70:
                 self.rect.y += 10
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-        
-
         
